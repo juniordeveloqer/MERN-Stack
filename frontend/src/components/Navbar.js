4d406a3b12bb2b8cdecac70219adb032899801ef
juniordeveloqer <antik.iskele@gmail.com>
2024-03-04 16:53:56 +0300

    commit 1

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>WORKOUT BUDDY</h1>
+          ur trasshh
+        </Link>
+      </div>{" "}
+    </header>
+  );
+};
+
+export default Navbar;
